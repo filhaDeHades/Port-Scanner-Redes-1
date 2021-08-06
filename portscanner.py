c7094c37d916692575aa1f3e4f168ed454e2ad30
Alan Gomes <ALNDSG@hotmail.com>
2021-08-06 17:21:43 -0300

    breve introdução

--- portscanner.py
+++ portscanner.py
@@ -1,1 +1,23 @@
-from socket import socket
+from socket import *
+
+# Variáveis do servidor
+serverName = "localhost"
+serverPort = 80
+
+# Variáveis do socket
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+setence = raw_input("Digite a mensagem:")
+
+# Envio de dados
+clientSocket.sendto(setence, (serverName,serverPort))
+
+# Envio de dados
+clientSocket.sendto(setence, (serverName,serverPort))
+
+# Resposta
+maxsizeMenssage = 1024 * 1 # tamanho em bytes
+modifieldSetence, addr = clientSocket.recvfrom(maxsizeMenssage) # resposta,
+print("From Server {}: {}".format(addr, modifieldSetence))
+
+# Fechar sockets
+clientSocket.close()
